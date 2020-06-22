6dc3e563bc7a8ce9cc7d7a0e76cdbff667ed4a90
SanjeevaAutomation <sanjeeva.testing999@gmail.com>
2020-06-22 15:06:36 +0530

    toady added at 15:00

--- TestProject/src/TestPakage/Test.java
+++ TestProject/src/TestPakage/Test.java
@@ -1,18 +1,27 @@
 package TestPakage;
-
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class Test {
-	public static boolean isElementSelected(WebElement element, boolean expStatus) {
-		boolean bStatus = false;
-		try {
-			highlight(element);
-			boolean b = element.isSelected();
-			if(b == expStatus) bStatus = true;
-		}
-		catch(Exception e) {
-			bStatus = false;
-		}
-		return bStatus;
+import java.util.concurrent.TimeUnit;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+	
+System.setProperty("webdriver.gecko.driver", "D:\\geckodriver.exe");
+System.setProperty("webdriver.firefox.bin","C:\\Users\\AppData\\Local\\Mozilla Firefox\\firefox.exe");
+		
+		FirefoxDriver driver=new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("");
+		
+		String rowData = driver.findElement(By.xpath("//*[@id='leftcontainer']/table/tbody/tr[3]/td[4]")).getText();
+		System.out.println("data of row data : "+rowData);
+		driver.findElement(By.xpath("//*[@id='leftcontainer']/table/tbody/tr[3]/td[]"));
+		System.out.println("data of row is : "+rowData);
+		
 	}
+
 }
