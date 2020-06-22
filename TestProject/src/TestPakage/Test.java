b6dfb1246c5582cb81f631314981cb070613f1d6
SanjeevaAutomation <sanjeeva.testing999@gmail.com>
2020-06-22 14:50:02 +0530

    added on 22062020

--- TestProject/src/TestPakage/Test.java
+++ TestProject/src/TestPakage/Test.java
@@ -1,5 +1,18 @@
 package TestPakage;
 
-public class Test {
+import org.openqa.selenium.WebElement;
 
+public class Test {
+	public static boolean isElementSelected(WebElement element, boolean expStatus) {
+		boolean bStatus = false;
+		try {
+			highlight(element);
+			boolean b = element.isSelected();
+			if(b == expStatus) bStatus = true;
+		}
+		catch(Exception e) {
+			bStatus = false;
+		}
+		return bStatus;
+	}
 }
